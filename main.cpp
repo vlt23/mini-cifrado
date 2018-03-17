d18b8c48e83e3db20c2d90073e7d1d4bbb32c3c1
vlt23 <32304181+vlt23@users.noreply.github.com>
2018-03-17 18:28:13 +0100

    MiniCifrado de mensajes

---
+++ main.cpp
@@ -0,0 +1,74 @@
+/*
+ * Author: vlt23
+ * Creation Date: 2018/03/17
+ * Last Modified: 2018/03/17
+ * Cadena de caracteres cifrada con los siguientes criterios:
+    * Los caracteres que no sean letras no se modifican.
+    * Cada consonantes se sustituye por la siguiente consonante en el
+        abecedario (la siguiente a la ‘Z’ es la ‘B’).
+    * Cada secuencia de vocales consecutivas se sustituye por la misma
+        secuencia de vocales pero en orden inverso.
+ * License: GNU GPLv3
+ *
+ * TODO: ---
+ * Bugs: ¿?
+ */
+
+#include <iostream>
+#include "arrayqueue.h"
+#include "arraystack.h"
+
+using namespace std;
+
+int main()
+{
+    string mensaje;
+
+    ArrayQueue<char> colaEntrada;
+    ArrayQueue<char> colaSalida;
+    ArrayStack<char> pila;
+
+    getline(cin, mensaje);
+    for (size_t i = 0; i < mensaje.length(); i++)
+    {
+        colaEntrada.push_back(mensaje[i]);
+    }
+
+    while (!colaEntrada.isEmpty())
+    {
+        char letra = colaEntrada.front();
+        // las vocales se apilan...
+        if (letra == 'a' || letra == 'e' || letra == 'i'
+                || letra == 'o' || letra == 'u')
+        {
+            pila.push(letra);
+        }
+        else
+        {
+            // ... para sustituir en orden inverso
+            while (!pila.isEmpty())
+            {
+                colaSalida.push_back(pila.top());
+                pila.pop();
+            }
+            char next = letra + 1;
+            // la siguiente a la 'z' es la 'b', NO la 'a'
+            if (next == 'a' || next == 'e' || next == 'i'
+                    || next == 'o' || next == 'u')
+            {
+                next = next + 1;
+            }
+            colaSalida.push_back(next);
+        }
+        colaEntrada.pop_front();
+    }
+
+    while (!colaSalida.isEmpty())
+    {
+        cout << colaSalida.front();
+        colaSalida.pop_front();
+    }
+    cout << endl;
+
+    return 0;
+}
